ab28b193b6b8c7850b4c8cdcd32a62db0181236a
petropapahadjopoulos <anotherguywithabeanie@gmail.com>
2022-11-16 18:14:51 -0800

    test

--- Codeforces/mixingMilk.java
+++ Codeforces/mixingMilk.java
@@ -1,4 +1,7 @@
 public class mixingMilk {
+    public static int fillBucket(){
+        return -1;
+    }
     public static void main(String[] args) {
         int[] maxLiquid = new int[3];
         int[] curLiquid = new int[3];
