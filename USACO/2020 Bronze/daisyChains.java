c48f3452d7abd689be18559f2631f3ce033961e0
petropapahadjopoulos <anotherguywithabeanie@gmail.com>
2022-12-08 17:42:59 -0800

    Co-authored-by: John Carmichael <dgh1000@users.noreply.github.com>

--- USACO/2020 Bronze/daisyChains.java
+++ USACO/2020 Bronze/daisyChains.java
@@ -16,31 +16,23 @@ public class daisyChains{
         int[] petals = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         for(int i = 0; i < n; i++){
             int curPetals = 0;
-            System.out.println("outside loop");
             for(int j = i; j < n; j++){
                 // here we have determined (i, j)
                 // here we find sum of petals i to j
-                System.out.println("loop \n");
                 curPetals = 0;
                 for(int k = i; k <= j; k++){
                     curPetals += petals[k];
-                    System.out.print(petals[k] + " - ");
                 }
                 // another loop for check the average'
-                System.out.println();
                 int length = 0;
-                System.out.println(j + " = " + length + " - " + i);
                 for(; length <= j; length++){
                     if(j == 0){
                         total++;
-                        System.out.println("skipped");
                         break;
                     }
-                    System.out.println("inside loop 2 - " + curPetals + " - " + (petals[j] * length));
                     if(curPetals == petals[j] * length){
                         
                         total++;
-                        System.out.println("average flower");
                     }
                 }
             
